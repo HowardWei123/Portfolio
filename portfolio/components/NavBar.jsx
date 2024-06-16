1bb6e78ec84ab54526def0e63353edd7c422e62e
HowardWei123 <howieweiinf@gmail.com>
2024-06-16 13:12:14 -0700

    hm

--- portfolio/components/NavBar.jsx
+++ portfolio/components/NavBar.jsx
@@ -1,31 +1,70 @@
 import Image from "next/image"
+import Link from "next/link"
 import React from 'react'
 import {BsMoonStarsFill} from 'react-icons/bs'
+import {AiOutlineMenu} from 'react-icons/ai'
+import NavLogo from '../public/navImgs/navlogo.png'
 
 const NavBar = () => {
   return (
-    <nav className="px-6 py-6 mb-12 flex justify-between shadow-lg">
-      <div className="flex">
-        <h1 className="cursor-pointer text-xl font-bold">Howard Wei</h1>
-        <ul className="px-4 text-xl hidden md:flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
+    <nav className="fixed w-full h-20 mb-10">
+      <div className="flex justify-between w-full h-20 px-2 py-5 shadow-lg">
+        <div className="flex">
+          <Link href='/'>
+            <Image
+              src={NavLogo}
+              width={200}
+              height={200}
+              className="mx-4 cursor-pointer"
+            />     
+          </Link>
+
+          <div>
+            <ul className="ml-4 pt-1 hidden lg:flex">
+              <Link href='#projects'>
+                <li className="pb-4 transition duration-300 font-bold text-2xl hover:border-b-2 hover:border-blue-600 hover:text-blue-600">
+                  Projects
+                </li>
+              </Link>
+              <Link href='#about'>
+                <li className="pb-4 transition duration-300 font-bold text-2xl hover:border-b-2 hover:border-blue-600 hover:text-blue-600">
+                  About
+                </li>
+              </Link>
+              <Link href='#skills'>
+                <li className="pb-4 transition duration-300 font-bold text-2xl hover:border-b-2 hover:border-blue-600 hover:text-blue-600">
+                  Skills
+                </li>
+              </Link>
+              <Link href='#contact'>
+                <li className="pb-4 transition duration-300 font-bold text-2xl hover:border-b-2 hover:border-blue-600 hover:text-blue-600">
+                  Contact
+                </li>
+              </Link>
+            </ul>
+          </div>
+        </div>
+        
+        <div className="mt-2">
+          <ul className="flex items-center">
+            <div className="lg:hidden">
+              <li>
+                <AiOutlineMenu size={25}/>
+              </li>
+            </div>
+
+            <li className="pr-1">
+              <BsMoonStarsFill className="cursor-pointer text-2xl"/>
+            </li>
+            <li>
+              <Link className="transition duration-300 bg-cyan-500 hover:bg-blue-500 text-white px-4 py-2 rounded-md" href="/">
+                Resume
+              </Link>
+            </li>
+          </ul>
+        </div>
+        
       </div>
-      
-      <ul className="flex items-center">
-        <li>
-          <BsMoonStarsFill className="cursor-pointer text-2xl"/>
-        </li>
-        <li>
-          <a className="bg-cyan-500 text-white px-4 py-2 rounded-md" href="#">
-            Resume
-          </a>
-        </li>
-      </ul>
     </nav>
   )
 }
